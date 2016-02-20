2d315e12b96ecaaecf3f8f6ed797b9d0aeffdaac
zilp <shilpaksvn@gmail.com>
2016-02-20 15:54:25 -0500

    moved logic out of parser and into logic.py

--- logic.py
+++ logic.py
@@ -1,4 +1,32 @@
-# placeholder for all the logic functions
+# umbc api for semantic similarity
+sss_url = "http://swoogle.umbc.edu/SimService/GetSimilarity"
 
-def example():
-    print("Hello there friend.")
+def sss(s1, s2, type='relation', corpus='webbase'):
+    try:
+        response = get(sss_url, params={'operation':'api','phrase1':s1,'phrase2':s2,'type':type,'corpus':corpus})
+        return float(response.text.strip())
+    except:
+        print 'Error in getting similarity for %s: %s' % ((s1,s2), response)
+        return 0.0
+
+# find semantically similar words in present web text
+def trigger(txt, tgt):
+    trigs = [tgt]
+    for s in txt.split():
+        val = sss(tgt, s, type='relation', corpus=txt)
+        if val > 0.75:
+            trigs.add(s)
+    return trigs
+
+# count trigger words
+def count_trigs(txt, trigs):
+    ctr = 0
+    for t in trigs:
+        for s in txt.split():
+            if (s == t):
+                ctr = ctr + 1
+    return ctr
+
+# trigger words as a percentage of the whole text
+def trig_stats(txt):
+    ctr / len(txt.split())
--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ from requests import get
 import urllib
 from bs4 import BeautifulSoup
 
+# extract visible text from url
 def extract(url):
     html = urllib.urlopen(url).read()
     soup = BeautifulSoup(html, "xml")
@@ -12,45 +13,7 @@ def extract(url):
     #visible_text = filter(visible, raw)
     return text
 
-def finder(orig, target):
-    orig = orig.strip()
-    if len(orig) < 1 or not orig:
-        return "failed"
-    words = orig.split()
-    ctr = 0
-    for s in words:
-        if (s == target):
-            ctr = ctr + 1
-    return ctr
-
 web_text = extract("https://en.wikipedia.org/wiki/Fermi_paradox").split()
 
 for x in web_text:
     print x
-
-'''
-
-sss_url = "http://swoogle.umbc.edu/SimService/GetSimilarity"
-
-def sss(s1, s2, type='relation', corpus='webbase'):
-    try:
-        response = get(sss_url, params={'operation':'api','phrase1':s1,'phrase2':s2,'type':type,'corpus':corpus})
-        return float(response.text.strip())
-    except:
-        print 'Error in getting similarity for %s: %s' % ((s1,s2), response)
-        return 0.0
-'''
-
-'''
-def extra():
-    triggers = []
-
-    for s in sss.corpus:
-        val = sss(target, s)
-        if val > 0.75:
-            triggers.add(s)
-
-    overall_count = 0
-    for s in triggers:
-        overall_count = overall_count + finders(s)
-'''
