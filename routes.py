fc6eb3863ff2c88bbc6a2802157c0d1879b8bc10
rdiamon1 <rdiamon1@swarthmore.edu>
2016-02-20 15:56:34 -0500

    updated html with two text fields

--- routes.py
+++ routes.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+# We need to import request to access the details of the POST request
+# and render_template, to render our templates (form and response)
+# we'll use url_for to get some URLs for the app on the templates
+from flask import Flask, render_template, request, url_for
 
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Define a route for the default URL, which loads the form
 @app.route('/')
-def home():
-  return render_template('home.html')
+def form():
+    return render_template('home.html')
 
+# Define a route for the action of the form, for example '/hello/'
+# We are also defining which type of requests this route is
+# accepting: POST requests in this case
+@app.route('/hello/', methods=['POST'])
+def hello():
+    name=request.form['url']
+    email=request.form['keyword']
+    return render_template('form_action.html', name=name, email=email)
+"""
+# Run the app :)
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(
+        host="0.0.0.0",
+        port=int("5000")
+  )
+"""
+if __name__ == '__main__':
+    app.run(debug=True)
