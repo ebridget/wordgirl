e630b39f7e74bcd4fb2ce9f762d80779b79222fa
zilp <shilpaksvn@gmail.com>
2016-02-20 20:38:31 -0500

    reconfigured logic

--- logic.py
+++ logic.py
@@ -1,27 +1,29 @@
 from requests import get
 import logic
 
+
 # umbc api for semantic similarity
 sss_url = "http://swoogle.umbc.edu/SimService/GetSimilarity"
 
-# delete common words
-common_words = ["a", "an", "the", "but", "and", "or"]
-def del_words(w):
-    if w in common_words:
-        return False
-    else:
-        return True
 
 #find semantically similar words in text
 def similar_words(tgt, txt, type='relation', corpus='webbase'):
     trigs = set([tgt])
-    txt_smaller = filter(del_words, txt)
-    for s in txt_smaller:
+    not_trigs = set([])
+    ctr = 0
+    for s in txt:
         if s.isalpha():
-            response = get(sss_url, params={'operation':'api','phrase1':tgt,'phrase2':s,'type':type,'corpus':corpus})
-            if float(response.text.strip()) > 0.5:
-                trigs.add(s)
-    return trigs
+            if s in trigs:
+                ctr += 1
+            else:
+                if s not in not_trigs:
+                    response = get(sss_url, params={'operation':'api','phrase1':tgt,'phrase2':s,'type':type,'corpus':corpus})
+                    if float(response.text.strip()) > 0.5:
+                        trigs.add(s)
+                        ctr += 1
+                    else:
+                        not_trigs.add(s)
+    return trigs, ctr
 
 
 # count keyword and related words
@@ -35,4 +37,4 @@ def count_words(txt, trigs):
 
 # instances of keyword and related words as a percentage of the whole text
 def word_percent(txt, trigs):
-    count_words(txt, trigs) / len(txt)
+    return count_words(txt, trigs) / len(txt)
