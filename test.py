351a319e9e5cde79cbe3d39016670cbc8935745e
zilp <shilpaksvn@gmail.com>
2016-02-20 20:16:04 -0500

    fixed similar_words

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ url = raw_input("enter url: ")
 tgt = raw_input("what word don't you want to see?: ")
 
 txt = parser.extract(url)
-trigs = logic.trigger(txt, tgt)
-print logic.trig_stats(txt, trigs)
+trigs = logic.similar_words(tgt, txt)
+print trigs
